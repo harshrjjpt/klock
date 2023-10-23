ed8df6ac5814a8acd83d707703d2ad58d4375774
harshrjjpt <harshrjjpt@gmail.com>
2023-10-23 18:22:56 +0530

    media queries

--- src/app/components/AboutSection/AboutSection.tsx
+++ src/app/components/AboutSection/AboutSection.tsx
@@ -27,15 +27,15 @@ export default function About({}: AboutProps) {
   return (
     <section className={`${styles.aboutSection} ${scrolling ? styles.scrollBackground : ""}`}>
       <h1 className={styles.heroHeading}>
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>Content</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>for</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>brands</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>that</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>is</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>powerful,</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>evocative,</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>and</span>{" "}
-        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>unforgettable.</span>
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>EMBRACE</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>ELEGANCE</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>TRANSFORM</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>YOUR</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>PC</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>WITH</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>DIGITAL</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>WATCH</span>{" "}
+        <span className={`${styles.split} ${scrolling ? styles.scrollText : ""}`}>SCREENSAVERS</span>
       </h1>
     </section>
   );
